93548a77438d3a543b10370726e5e808255124c1
27funa <k_funasaki@icloud.com>
2020-06-26 13:12:46 +0900

    create header

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 $(function(){
-    $('.slideshow').each(function(){
+    $('.hero-container').each(function(){
         var $slides = $(this).find('img'),
             slideCount = $slides.length,
             currentIndex = 0;
 
         $slides.eq(currentIndex).fadeIn();
 
-        setInterval(showNextSlide, 7500);
+        setInterval(showNextSlide, 7000);
 
         function showNextSlide(){
             var nextIndex = (currentIndex + 1) % slideCount;
 
-            $slides.eq(currentIndex).fadeOut();
+            $slides.eq(currentIndex).fadeOut(1000);
 
-            $slides.eq(nextIndex).fadeIn();
+            $slides.eq(nextIndex).fadeIn(1000);
 
             currentIndex = nextIndex;
         }
